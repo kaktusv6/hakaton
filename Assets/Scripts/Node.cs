a1c064f335af2471e653b683e780ada7b0645b72
Vasiliy <kaktusv6@gmail.com>
2017-11-07 16:15:40 +1100

    Add assets

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+
+	public Node[] Nodes;
+	public Node Parent;
+	
+	// Use this for initialization
+	void Start () {
+		foreach (Node node in Nodes)
+		{
+			Debug.Log(node.gameObject.name);
+			node.Parent = this;
+		}	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
