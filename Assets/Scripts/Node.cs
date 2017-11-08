feb427da118ba5232b68115c1ed9ae91bf06f0ee
Basik <Basik-cs@mail.ru>
2017-11-08 22:13:40 +1000

    много изменений

--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 
 public class Node : MonoBehaviour
 {
+    private static int INF = 1000000;
     public Transform transform;
 	public Node[] Nodes;
 	private Node Parent;
+    private float weight;
+    private int prev;
+    private bool isOpen;
 	
 	// Use this for initialization
 	void Start () {
+        weight = INF;
+        prev = -1;
+        isOpen = false;
 		foreach (Node node in Nodes)
 		{
 			//Debug.Log(node.gameObject.name);
@@ -21,4 +28,30 @@ public class Node : MonoBehaviour
 	void Update () {
 		
 	}
+
+    public float getWeight()
+    {
+        return this.weight;
+    }
+    public void setWeight(float weight)
+    {
+        this.weight = weight;
+    }
+
+    public int getPrev()
+    {
+        return this.prev;
+    }
+    public void setPrev(int prev)
+    {
+        this.prev = prev;
+    }
+    public bool getIsOpen()
+    {
+        return this.isOpen;
+    }
+    public void setIsOpen(bool isOpen)
+    {
+        this.isOpen = isOpen;
+    }
 }
--- Assets/Scripts/Way/BuildWay.cs
+++ Assets/Scripts/Way/BuildWay.cs
@@ -1,32 +1,34 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BuildWay : MonoBehaviour {
 
-    public Node root;
-    //public Node tail;
+    private static string NODE= "Node";
 
-	void Start () {
-        createEdges(root);
-	}
+    public Node start;
+    public Node finish;
+    public int countNodes;
+    //private float[][] graf;
+    List<List<float>> graf;
+    List<bool> mark;
 
-    private void createEdges(Node node)
-    {
-        if(node == null)
+    void Start () {
+        //mark = new List<bool>();
+        /*for (int i = 0; i < this.countNodes; i++)
         {
-            return;
-        }
-        for(int i = 0; i < node.Nodes.Length; i++)
-        {
-            //Debug.Log(node.name);
+            mark.Add(false);
+        }*/
+        start.setWeight(0);
 
-            //Debug.Log(node.transform.position);
-            createEdge(node.transform.position, node.Nodes[i].transform.position);
-            createEdges(node.Nodes[i]);
-        }
+        initGraf();
+        //testSecond(start);
+        //printWay(finish);
         
-    }
+        //Debug.Log(shortWay());
+        //createEdges(root);
+	}
 
     private void createEdge(Vector3 head, Vector3 tail)
     {
@@ -54,5 +56,158 @@ public class BuildWay : MonoBehaviour {
     private Vector3 getBasis(Vector3 vector, float distance)
     {
         return new Vector3(vector.x / distance, vector.y / distance, vector.z / distance);
+    }
+
+
+    private void initGraf()
+    {
+        graf = new List<List<float>>();
+        List<float> row;
+
+        for (int i = 0; i < this.countNodes; i++)
+        {
+            row = new List<float>();
+            for (int j = 0; j < this.countNodes; j++)
+            {
+                row.Add(-1);
+            }
+            graf.Add(row);
+        }
+
+        for(int i = 0; i < this.countNodes; i++)
+        {
+            for(int j = 0; j < this.countNodes; j++)
+            {
+                if (i == j)
+                {
+                   continue;
+                }
+
+                if ((GameObject.Find(i.ToString()) == null) || (GameObject.Find(j.ToString()) == null)){
+                    continue;
+                }
+
+                Node nodeHead = GameObject.Find(i.ToString()).GetComponent("Node") as Node;
+                Node nodeTail = GameObject.Find(j.ToString()).GetComponent("Node") as Node;
+
+                if (graf[i][j] == -1)
+                {
+                    for(int z = 0; z < nodeHead.Nodes.Length; z++)
+                    {
+                        if(nodeHead.Nodes[z] == nodeTail)
+                        {
+                            graf[i][j] = pointDistance(nodeHead.transform.position, nodeTail.transform.position);
+                            graf[j][i] = graf[i][j];
+                            createEdge(nodeHead.transform.position, nodeTail.transform.position);
+                        }
+                    }
+                   
+                }
+
+            }
+        }
+
+    }
+    
+    
+    private void test(Node node)
+    {
+        //Debug.Log(node.name);
+        //Debug.Log("после");
+        mark[Int32.Parse(node.name)] = true;
+        for(int i = 0; i < countNodes; i++)
+        {
+            Node minWay = getMinNodeWay(node);
+            if (minWay == null) continue;
+            if (mark[Int32.Parse(minWay.name)] == true) continue;
+            test(minWay);
+        }
+
+        
+    }
+
+    private Node getMinNodeWay(Node node)
+    {
+        float buffer = 0;
+        int nodeCount = -1;
+
+
+        for(int i = 0; i < countNodes; i++)
+        {
+           
+            GameObject gameObject = GameObject.Find(i.ToString());
+            if (gameObject == null) continue;
+       
+            Node nodeBuf = gameObject.GetComponent("Node") as Node;
+
+            if((nodeBuf.getWeight() > (node.getWeight()+ graf[Int32.Parse(node.name)][i])) && ((graf[Int32.Parse(node.name)][i]) != -1))
+            {
+                nodeBuf.setWeight(node.getWeight() + graf[Int32.Parse(node.name)][i]);
+                nodeBuf.setPrev(Int32.Parse(node.name));
+
+                if (((graf[Int32.Parse(node.name)][i] < buffer) || (buffer == 0)) && (mark[i] == false))
+                {
+                    buffer = graf[Int32.Parse(node.name)][i];
+                    nodeCount = i;
+                }
+            }
+            
+        }
+
+        if (nodeCount == -1)
+        {
+            return null;
+        }
+        Node newNode = GameObject.Find(nodeCount.ToString()).GetComponent("Node") as Node;
+        return newNode;
+    }
+
+    private void printWay(Node node)
+    {
+        Debug.Log(node.name);
+        GameObject gameObject = GameObject.Find(node.getPrev().ToString());
+        if (gameObject == null)
+        {
+            Debug.Log("ошибка");
+            return;
+        }
+        Node next = gameObject.GetComponent("Node") as Node;
+
+        createEdge(next.transform.position, node.transform.position);
+        printWay(next);
+    }
+
+
+    private void testSecond(Node start) 
+    {
+        Node node = start;
+
+        for (int i = 0; i < countNodes; i++)
+        {
+            GameObject gameObject = GameObject.Find(i.ToString());
+            if (gameObject == null) continue;
+            Node bufferNode = gameObject.GetComponent(NODE) as Node;
+
+            if((node.getWeight() > bufferNode.getWeight()) && (node.getIsOpen() == false ))
+            {
+                node = bufferNode;
+            }
+        }
+
+        step(node);
+    }
+
+    private void step(Node node)
+    {
+        node.setIsOpen(true);
+        for(int i = 0; i < countNodes; i++)
+        {
+            GameObject gameObject = GameObject.Find(i.ToString());
+            if (gameObject == null) continue;
+            Node bufferNode = gameObject.GetComponent(NODE) as Node;
+
+            //float sum = 
+            //if()
+        }
     }
 }
